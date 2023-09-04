5b4e05e77b02db5b150b4fafc5f3bb1a7220f8f7
Maxwell <maxwell.nganga@student.moringaschool.com>
2023-09-04 13:28:25 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age,sex, color, temperament, alive) VALUES
+    ("Mr.Chocolate", 20, "M", "white", "chubby", 1),
+    ("Rowdy", 35, "M", "Black", "geek", 1),
+    ("Tabitha", 17, "F", "brown", "Cute", 1),
+    ("Sergeant", 55, "M", "white", "Buff", 0),
+    ("Melissa", 22, "F", "Black", "Curvy", 1),
+    ("Grinch", 77, "M", "Greenish", "Skinny", 0),
+    ("Wendy", 32, "F", "Blonde", "Karen", 1),
+    (NULL, 43, "F", "Asian", "Joyvial", 0);
